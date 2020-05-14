ffc933e2de45274643d06c93693acadb62e495d1
Wen Liang Goh <wenlianggg@gmail.com>
2020-05-14 22:04:43 +0800

    Update thetris.py

--- snippets/thetris/thetris.py
+++ snippets/thetris/thetris.py
@@ -6,7 +6,7 @@ import random
 TICK_TIME_MILLIS = 500
 GAME_HEIGHT = 16
 GAME_WIDTH = 10
-KEYMAP = { 72: 'U', 75: 'L', 80: 'D', 77: 'R', 0: '-' }
+KEYMAP = { 72: 'U', 75: 'L', 80: 'D', 77: 'R' }
 BLOCK_CTR = { 'I': 0, 'J': 0, 'L': 0, 'O': 0, 'S': 0, 'T': 0, 'Z': 0 }
 BLOCK_BASE = { 'I': 1000, 'J': 2000, 'L': 3000, 'O': 4000, 'S': 5000, 'T': 6000, 'Z': 7000 }
 (   GAME_VALID, GAME_STARTED, GAME_OVER, 
@@ -16,12 +16,11 @@ BLOCK_BASE = { 'I': 1000, 'J': 2000, 'L': 3000, 'O': 4000, 'S': 5000, 'T': 6000,
 
 def main():
     grid = []
-    direction = 'D'
-    curr_block_id = None
-    curr_block_type = None
-    next_block_id = None
-    next_block_type = None
+    points = 0; cleared = 0
+    curr_block_id = None; curr_block_type = None
+    next_block_id = None; next_block_type = None
     game_state = GAME_STARTED
+    direction = 'D'
 
     for _ in range(GAME_HEIGHT):
         grid.append([0, ] * GAME_WIDTH)
@@ -47,20 +46,22 @@ def main():
         if game_state == GAME_OVER:
             print("Game over...")
             return
-
-        displaygame(grid=grid, curr=curr_block_type, next=next_block_type)
-        print(game_state)
-
+        
+        grid, newpoints, newcleared = clear_completed_lines(grid, cleared // 10)
+        points += newpoints
+        cleared += newcleared
+        displaygame(grid, curr_block_type, next_block_type, cleared // 10, points)
         usermove = readInput('Entered move', 'D', TICK_TIME_MILLIS)
-
-        if usermove is not 0:
-            game_state = USER_MOVED
+        if usermove is 27:
+            print(" - User ended the game")
+            game_state = GAME_OVER
+        if usermove in list(KEYMAP.keys()):
             direction = KEYMAP[usermove]
-            print(direction)
+            game_state = USER_MOVED
         else:
             direction = 'D'
 
-def displaygame(grid: list, curr: str, next: str):
+def displaygame(grid: list, curr: str, next: str, level: int, points: int):
     for i in range(GAME_HEIGHT):
         print("|", end='')
         for item in grid[i]:
@@ -69,18 +70,65 @@ def displaygame(grid: list, curr: str, next: str):
             else:
                 print(f'[{item:04d}]', end='')
         print("|", end='')
+        if i == 2:
+            print("    Score", end='')
         if i == 3:
-            print("    Current Block", end='')
+            print("   ", points, end='')
         if i == 4:
-            print("   ", curr, end='')
+            print("    Level", end='')
         if i == 5:
-            print("    Next Piece", end='')
+            print("   ", level, end='')
         if i == 6:
+            print("    Current Block", end='')
+        if i == 7:
+            print("   ", curr, end='')
+        if i == 8:
+            print("    Next Piece", end='')
+        if i == 9:
             print("   ", next, end='')
         print()
     print(" ⁠—⁠—⁠—⁠—⁠—⁠—⁠—⁠—⁠—⁠— \n")
 
+def clear_completed_lines(grid: list, level: int):
+    '''
+    Clears the grid of lines that are completed.\n
+    Returns the *new grid*, *number of points* the user would have gotten and the *number of lines cleared*
+    '''
+    lines_to_remove = []
+    for y in range(GAME_HEIGHT):
+        for x in range(GAME_WIDTH):
+            if grid[y][x] == 0:
+                break
+            if x + 1 == GAME_WIDTH:
+                lines_to_remove.append(y)
+
+    if len(lines_to_remove) == 0:
+        return grid, 0, 0
+
+    for y in lines_to_remove:
+        print("There are lines to remove!", lines_to_remove)
+        grid = [v for i, v in enumerate(grid) if i not in lines_to_remove]
+
+    for _ in lines_to_remove:
+        grid.insert(0, [0, ] * GAME_WIDTH)
+
+    if len(lines_to_remove) == 1:
+        return grid, 40 * (level + 1), len(lines_to_remove)
+    elif len(lines_to_remove) == 2:
+        return grid, 100 * (level + 1), len(lines_to_remove)
+    elif len(lines_to_remove) == 2:
+        return grid, 400 * (level + 1), len(lines_to_remove)
+    elif len(lines_to_remove) == 2:
+        return grid, 1200 * (level + 1), len(lines_to_remove)
+    else:
+        return grid, 0, 0
+
+
 def get_next_block():
+    '''
+    Generates a new blockid and blocktype randomly
+    First digit of blockid tells the block type
+    '''
     blocktypes_avail = list(BLOCK_CTR.keys())
     blocktype = random.choice(blocktypes_avail)
     blockid = BLOCK_BASE[blocktype] + (BLOCK_CTR[blocktype] % 1000 + 1)
@@ -94,21 +142,27 @@ def grid_block_move(grid, blockid: int, direction):
     for y in range(GAME_HEIGHT):
         for x in range(GAME_WIDTH):
             if grid[y][x] == blockid:
-
-                if y + 1 >= GAME_HEIGHT:
-                    print("Block bottomed on the base")
-                    return BLOCK_BOTTOMED
-
                 thiscoord = [x, y]
                 newcoord = [None, None]
                 
                 # Identify new coordinates for respective movement
                 if direction == 'L':
                     newcoord = [thiscoord[0] - 1, thiscoord[1]]
+                    if newcoord[0] < 0:
+                        print("Block movement blocked by wall")
+                        return BLOCK_MOVE_FAIL
+
                 if direction == 'D':
                     newcoord = [thiscoord[0], thiscoord[1] + 1]
+                    if newcoord[1] >= GAME_HEIGHT:
+                        print("Block bottomed on the base")
+                        return BLOCK_BOTTOMED
+
                 if direction == 'R':
                     newcoord = [thiscoord[0] + 1, thiscoord[1]]
+                    if newcoord[0] >= GAME_WIDTH:
+                        print("Block movement blocked by wall")
+                        return BLOCK_MOVE_FAIL
 
                 if direction in ['L', 'D', 'R'] and (grid[newcoord[1]][newcoord[0]] == 0 or grid[newcoord[1]][newcoord[0]] == blockid):
                     block_locations.append(thiscoord)
